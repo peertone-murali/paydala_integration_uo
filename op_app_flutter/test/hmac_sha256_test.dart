c07c154aba756c046dc6527a5f8c37c7dfb84498
Muralidhara K <murali@paydala.com>
2023-03-21 10:58:46 +0530

    added test for generateHmacSha256Signature

---
+++ op_app_flutter/test/hmac_sha256_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:op_app_flutter/src/utils.dart';
+
+void main() {
+  test('HMAC with Java', () {
+    const data = 'Hello World!';
+    const key = '1fc2b5832bf04c5599476f24c8d86ab8';
+
+    const expectedOutput =
+        '9a20ddec8ae972bb18d57c0b18e9063486bc023dfea946f4f9f282db315d9e74';
+
+    final output = generateHmacSha256Signature(data); //, key);
+    if (kDebugMode) {
+      print("expectedOutput = $expectedOutput, actualOutput = $output");
+    }
+
+    expect(output, equals(expectedOutput));
+  });
+}
